df3151f23f535f2c3132e341e7abd1258d475ebd
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-10-27 20:44:58 +0530

    fetches

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,71 @@
+import React, { useState, useEffect } from "react";
 
-import React from "react";
-import './../styles/App.css';
+const APIURL = "https://reqres.in/api/users";
 
-const App = () => {
+function App() {
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(APIURL);
+      if (response.ok) {
+        const data = await response.json();
+        setUserList(data.data);
+      } else {
+        console.error("Failed to fetch user data.");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <main>
+      <h1>User List</h1>
+      <div>
+        <button className="btn" onClick={getUsers}>
+          Get User List
+        </button>
+      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Email</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.email}</td>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>
+                  <img src={user.avatar} alt={`Avatar for ${user.first_name}`} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
